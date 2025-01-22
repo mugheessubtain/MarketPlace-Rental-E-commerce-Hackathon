42ea35ab3b4c79d526e3bfe20c2011c1946bbb5b
Mughees Subtain <160583245+mugheessubtain@users.noreply.github.com>
2025-01-21 18:34:18 -0800

    Update Reviews.tsx

--- components/major/Reviews.tsx
+++ components/major/Reviews.tsx
@@ -23,17 +23,7 @@ const reviews: Review[] = [
     date: "15 Jan 2024",
     rating: 5,
     content: "We are very happy with this service from the MORENT App. Morent has a low price and also a large variety of cars with good and comfortable facilities. In addition, the service provided by the officers is also very friendly and very polite. Wishing you the best!",
-    image: "/profile.png"
-  },
-  {
-    id: 2,
-    name: "Saifullah Abdullah",
-    position: "Founder",
-    company: "Center of Chemicals",
-    date: "15 August 2024",
-    rating: 4,
-    content: "We are greatly helped by the services of the MORENT Application. Morent has low prices and also a wide variety of cars with good and comfortable facilities. In addition, the service provided by the officers is also very friendly and very polite. Higly Recommended!",
-    image: "/cars/safeImg.png"
+    image: "/Profile.png"
   }
 ]
 
